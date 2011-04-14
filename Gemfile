e5998c9a429202102f3adaca819532c29e4c8d76
Igor Alexandrov <igor.alexandrov@gmail.com>
2011-04-15 00:36:33 +0400

    Gems added

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :gemcutter
+
+gem "rails", "~> 2.3.11"
+gem "sqlite3-ruby", :require => "sqlite3"
+gem 'haml'
+gem 'holidays'
+gem 'mongrel'
--- config/environment.rb
+++ config/environment.rb
@@ -14,10 +14,6 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.autoload_paths += %W( #{RAILS_ROOT}/extras )
 
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'haml'
-  config.gem 'holidays'
-  
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
